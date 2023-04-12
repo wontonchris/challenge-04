031d5faca1027fd63d7800352ccbea693dd10e0c
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-04-12 15:01:26 -0500

    lord jesus

--- script.js
+++ script.js
@@ -1,110 +1,11 @@
-// // Declare variables
-// var time = 60;
-// var timer;
-// var currentQuestion = 0;
-// var score = 0;
-
-// // Get DOM elements
-// var startBtn = document.getElementById("start");
-// var questionEls = document.querySelectorAll(".question");
-// var quizEl = document.querySelector(".quiz");
-// var timeEl = document.getElementById("time");
-// var scoreEl = document.getElementById("score");
-// var initialsEl = document.getElementById("initials");
-// var submitBtn = document.getElementById("submit");
-// var scoreForm = document.getElementById("score-form");
-
-// // Start quiz when start button is clicked
-// startBtn.addEventListener("click", startQuiz);
-
-// // Function to start quiz
-// function startQuiz() {
-//   // Hide start button
-//   startBtn.style.display = "none";
-//   // Show first question
-//   showQuestion(currentQuestion);
-//   // Start timer
-//   timer = setInterval(countdown, 1000);
-// }
-
-// // Function to display question
-// function showQuestion(index) {
-//   // Hide all questions
-//   questionEls.forEach(function(question) {
-//     question.style.display = "none";
-//   });
-//   // Display current question
-//   questionEls[index].style.display = "block";
-// }
-
-// // Function to handle answer selection
-// function selectAnswer() {
-//   // Check if answer is correct
-//   if (this.value === questionEls[currentQuestion].querySelector("input[type=hidden]").value ) {
-//     score++;
-//   } else {
-//     // Deduct 5 seconds for wrong answer
-//     time -= 5;
-//   }
-//   // Check if quiz is over
-//   currentQuestion++;
-//   if (currentQuestion === questionEls.length || time <= 0) {
-//     endQuiz();
-//   } else {
-//     showQuestion(currentQuestion);
-//   }
-// }
-
-// // Function to end quiz
-// function endQuiz() {
-//   // Stop timer
-//   clearInterval(timer);
-//   // Hide quiz
-//   quizEl.style.display = "none";
-//   // Show score and initials form
-//   scoreEl.textContent = "Score: " + score;
-//   document.querySelector(".score").style.display = "block";
-// }
-
-// // Function to handle timer countdown
-// function countdown() {
-//   time--;
-//   timeEl.textContent = "Time Remaining: " + time + " seconds";
-//   if (time <= 0) {
-//     endQuiz();
-//   }
-// }
-
-// // Add event listener to each answer input
-// var answerInputs = document.querySelectorAll("input[type=radio]");
-// answerInputs.forEach(function(input) {
-//   input.addEventListener("click", selectAnswer);
-// });
-
-// // Handle submission of initials and score
-// scoreForm.addEventListener("submit", function(event) {
-//   event.preventDefault();
-//   // Save initials and score to local storage
-//   var initials = initialsEl.value.toUpperCase();
-//   var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
-//   highScores.push({
-//     initials: initials,
-//     score: score
-//   });
-//   localStorage.setItem("highScores", JSON.stringify(highScores));
-//   // Redirect to high scores page
-//   window.location.href = "highscores.html";
-// });
-
-var startQuizButtonElement = document.getElementById("start-button");
-// var questionContainerElement = document.getElementById("question-container");
-// var answerContainerElement = document.getElementById("answer-container");
 var currentQuestion = 0;
+var timeLeft = 30;
+var userScore = 0;
 
 var questionsList = [
   {
-    questionText: ">Where was I born?",
-    choices: [" Taiwan", "Hong Kong", "USA", "China"],
+    questionText: "Where was I born?",
+    choices: ["Taiwan", "Hong Kong", "USA", "China"],
     correctAnswer: "USA",
   },
   {
@@ -113,74 +14,107 @@ var questionsList = [
     correctAnswer: "Cantonese",
   },
   {
-    questionText:
-      "Who is my favorite Artist?",
+    questionText: "Who is my favorite Artist?",
     choices: ["Boris Brejcha", "FKJ", "Diplo", "Ark Patrol"],
-    CorrectAnswer: "Boris Brejcha",
+    correctAnswer: "Boris Brejcha",
   },
   {
-    questionText: 'Who is the GOAT?',
+    questionText: "Who is the GOAT?",
     choices: ["Lebron James", "Michael Jordan", "Kobe Bryant", "Kareem Abdul-Jabbar"],
-    CorrectAnswer: "Lebron James",
+    correctAnswer: "Lebron James",
   },
   {
     questionText: "What is the best car manufacturer?",
     choices: ["BMW", "Porsche", "Ferrari", "Lamborghini"],
-    CorrectAnswer: "Ferrari",
-  },
-
-
+    correctAnswer: "Ferrari",
+  }
 ];
 
-// function startQuiz() {
-//   console.log("I click the button");
-// }
-// console.log(startQuizButtonElement);
-// startQuizButtonElement.addEventListener("click", startQuiz);
+var startQuizButtonElement = document.getElementById("start-button");
+startQuizButtonElement.addEventListener("click", startQuiz);
 
-// function startQuiz() {
-//   console.log("I click the button");
-//   startQuizButtonElement.style.display = "none";
-//   createQuizStage();
-//   createQuestionElements();
-// }
-// function createQuizStage() {
-//   var quizContainer = document.createElement("article");
-//   quizContainer.classList.add("quiz-container");
-//   document.body.appendChild(quizContainer);
-// }
-// function createQuestionElements(currentQuestionData) {
-//   var currentQuestionText = currentQuestionData.question;
-//   var questionTextBox = document.createElement("h3");
-//   var choiceOne = document.createElement("button");
-//   var choiceTwo = document.createElement("button");
-//   var choiceThree = document.createElement("button");
-//   var choiceFour = document.createElement("button");
-//   var choiceContainer = document.createElement("section");
-//   questionTextBox.textContent = "Question text goes here!";
-//   choiceOne.textContent = "choice 1";
-//   choiceTwo.textContent = "choice 2";
-//   choiceThree.textContent = "choice 3";
-//   choiceFour.textContent = "choice 4";
-//   var quizContainer = document
-//     .querySelector(".quiz-container")
-//     .appendChild(questionTextBox);
-//   quizContainer.appendChild(choiceContainer);
-//   choiceContainer.appendChild(choiceOne);
-//   choiceContainer.appendChild(choiceTwo);
-//   choiceContainer.appendChild(choiceThree);
-//   choiceContainer.appendChild(choiceFour);
-// }
-
-var questionsTextElement = document.getElementById("questions-text");
-var choicesContainerElement = document.getElementById("choices-container")
-
-
-function main(){ 
-    questionsTextElement.textContent = questionsList[0].questionText;
-    for(var i = 0; i < questionsList[0].choices.length; i++){
-        var newChoiceButton = document.createElement("button");
-        newChoiceButton.textContent = questionsList[0].choices[i];
-        choicesContainerElement.append(newChoiceButton);
+function startQuiz() {
+  console.log("I click the button");
+  startQuizButtonElement.style.display = "none";
+  createQuestionElements(questionsList[currentQuestion]);
+  setInterval(function() {
+    timeLeft--;
+    document.getElementById("timer-container").innerHTML = "Time left: " + timeLeft;
+    if (timeLeft <= 0) {
+      clearInterval();
+      alert("Time's up!");
     }
+  }, 1000);
 }
+
+function createQuizStage() {
+  var quizContainer = document.createElement("article");
+  quizContainer.classList.add("quiz-container");
+  document.body.appendChild(quizContainer);
+}
+
+function createQuestionElements(currentQuestionData) {
+  var currentQuestionText = currentQuestionData.questionText;
+  var questionTextBox = document.createElement("h3");
+  var choiceOne = document.createElement("button");
+  var choiceTwo = document.createElement("button");
+  var choiceThree = document.createElement("button");
+  var choiceFour = document.createElement("button");
+  var choiceContainer = document.createElement("section");
+  questionTextBox.textContent = currentQuestionText;
+  choiceOne.textContent = currentQuestionData.choices[0];
+  choiceTwo.textContent = currentQuestionData.choices[1];
+  choiceThree.textContent = currentQuestionData.choices[2];
+  choiceFour.textContent = currentQuestionData.choices[3];
+  var quizContainer = document.querySelector(".quiz-container");
+  quizContainer.appendChild(questionTextBox);
+  quizContainer.appendChild(choiceContainer);
+  choiceContainer.appendChild(choiceOne);
+  choiceContainer.appendChild(choiceTwo);
+  choiceContainer.appendChild(choiceThree);
+  choiceContainer.appendChild(choiceFour);
+  choiceOne.addEventListener("click", function() {
+    checkAnswer(choiceOne, currentQuestionData);
+  });
+  choiceTwo.addEventListener("click", function() {
+    checkAnswer(choiceTwo, currentQuestionData);
+  });
+  choiceThree.addEventListener("click", function() {
+    checkAnswer(choiceThree, currentQuestionData);
+  });
+  choiceFour.addEventListener("click", function() {
+    checkAnswer(choiceFour, currentQuestionData);
+  });
+ }
+
+ function checkAnswer(selectedChoice, currentQuestionData) {
+  if (selectedChoice.textContent === currentQuestionData.correctAnswer) {
+    console.log("Correct answer");
+    userScore += 20;
+  }
+  currentQuestion++;
+  if (currentQuestion < questionsList.length) {
+    createQuestionElements(questionsList[currentQuestion]);
+  } else {
+    console.log("Quiz completed");
+    displayScore();
+  }
+}
+
+setInterval(function() {
+  timeLeft--;
+  document.getElementById("timer-container").innerHTML = "Time left: " + timeLeft;
+  if (timeLeft <= 0) {
+    clearInterval();
+    alert("Time's up!");
+    displayScore();
+  }
+}, 1000);
+
+function displayScore() {
+  var initials = prompt("Congratulations! You finished with a score of " + userScore + ". Please enter your initials to save your score:");
+  localStorage.setItem(initials, userScore);
+  window.location.href = "highscores.html";
+}
+
+
